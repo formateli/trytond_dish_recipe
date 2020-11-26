1af1a4e318e1710dc602c81d585695dbcb027209
formateli <formateli@gmail.com>
2020-11-25 19:04:21 -0500

    Category parent domain

--- category.py
+++ category.py
@@ -1,15 +1,17 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields, tree
-
-__all__ = ['Category']
+from trytond.pyson import Eval
 
 
 class Category(tree(separator=' / '), ModelSQL, ModelView):
     "Recipe Category"
     __name__ = "dish_recipe.category"
     name = fields.Char('Name', required=True, translate=True)
-    parent = fields.Many2One('dish_recipe.category', 'Parent', select=True)
+    parent = fields.Many2One('dish_recipe.category', 'Parent', select=True,
+        domain=[
+            ('id', '!=', Eval('id')),
+        ], depends=['id'])
     childs = fields.One2Many('dish_recipe.category', 'parent',
             string='Children')
     recipes = fields.One2Many('dish_recipe.recipe',
