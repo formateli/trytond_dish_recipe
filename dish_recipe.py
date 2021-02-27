e9339f2b0b3b4fa3f6b87e1804ba5047dbc9683b
formateli <formateli@gmail.com>
2021-02-27 16:54:24 -0500

    Include tax in cost

--- dish_recipe.py
+++ dish_recipe.py
@@ -7,6 +7,7 @@ from trytond.pyson import Bool, Eval, If
 from trytond.modules.company.model import (
     CompanyMultiValueMixin, CompanyValueMixin)
 from trytond.modules.product import price_digits
+from trytond.modules.account.tax import _TaxKey
 from decimal import Decimal
 
 
@@ -226,6 +227,7 @@ class RecipeComponent(ModelSQL, ModelView):
         fields.Many2One('product.uom.category', 'Product Uom Category'),
         'on_change_with_product_uom_category')
     waste = fields.Float('Waste Percentage', digits=price_digits)
+    taxes = fields.Boolean('Include taxes')
     cost = fields.Function(fields.Numeric('Cost',
             digits=price_digits),
         'on_change_with_cost')
@@ -244,28 +246,11 @@ class RecipeComponent(ModelSQL, ModelView):
         if self.product:
             return self.product.default_uom_category.id
 
-    #def get_cost(self, name=None):
-    #    Uom = Pool().get('product.uom')
-    #    if not self.product or not self.unit:
-    #        return Decimal('0.0')
-    #    cost = Uom.compute_price(
-    #        self.product.default_uom,
-    #        self.product.cost_price,
-    #        self.unit)
-    #    return cost
-
-    @fields.depends('product', 'unit')
+    @fields.depends('product', 'unit', 'taxes')
     def on_change_with_cost(self, name=None):
-        if not self.product or not self.unit:
-            return Decimal('0.0')
-        Uom = Pool().get('product.uom')
-        cost = Uom.compute_price(
-            self.product.default_uom,
-            self.product.cost_price,
-            self.unit)
-        return cost
+        return self._calculate_cost(self.product, self.unit, self.taxes)
 
-    @fields.depends('quantity', 'cost', 'waste')
+    @fields.depends('quantity', 'cost', 'waste', 'taxes')
     def on_change_with_total_cost(self, name=None):
         if not self.quantity:
             return Decimal('0.0')
@@ -275,20 +260,81 @@ class RecipeComponent(ModelSQL, ModelView):
             total += waste
         return total
 
-    #def get_total_cost(self, name=None):
-    #    if not self.quantity:
-    #        return Decimal('0.0')
-    #   total = self.cost * Decimal(self.quantity)
-    #    if self.waste and (self.waste > 0 and self.waste < 100):
-    #        waste = total * (self.waste / 100)
-    #        total += waste
-    #    return total
+    def _calculate_cost(self, product, unit, include_tax):
+        if not product or not unit:
+            return Decimal('0.0')
+        Uom = Pool().get('product.uom')
+        cost = Uom.compute_price(
+            product.default_uom,
+            product.cost_price,
+            unit)
 
-    #@fields.depends('product', 'unit', 'quantity')
-    #def on_change_quantity(self):
-    #    self.cost = self.get_cost()
-    #    self.total_cost = self.get_total_cost()
+        res = cost
+        if include_tax:
+            tax_amount = self._compute_taxes(product, cost)
+            res = cost + tax_amount
+        return res
 
-    #@fields.depends(methods=['on_change_quantity'])
-    #def on_change_unit(self):
-    #    self.on_change_quantity()
+    def _compute_taxes(self, product, cost):
+        pool = Pool()
+        Tax = pool.get('account.tax')
+        Company = pool.get('company.company')
+
+        tax_ids = []
+        taxes = {}
+        currency = None
+
+        company_id = Transaction().context.get('company', None)
+        if company_id is not None:
+            currency = Company(company_id).currency
+
+        print(product.name)
+
+        for tax in product.supplier_taxes_used:
+            tax_ids.append(tax.id)
+
+        l_taxes = Tax.compute(Tax.browse(tax_ids), cost, 1, None)
+        #print(l_taxes)
+
+        for tax in l_taxes:
+            taxline = self._compute_tax_line(**tax)
+            # Base must always be rounded per line as there will be one
+            # tax line per taxable_lines
+            if currency is not None:
+                taxline['base'] = currency.round(taxline['base'])
+            if taxline not in taxes:
+                taxes[taxline] = taxline
+            else:
+                taxes[taxline]['base'] += taxline['base']
+                taxes[taxline]['amount'] += taxline['amount']
+
+        self._round_taxes(taxes, currency)
+        res = Decimal('0.0')
+        for tx in taxes.items():
+            res = tx[0]['amount']
+        return res
+
+    @staticmethod
+    def _compute_tax_line(amount, base, tax):
+        if base >= 0:
+            type_ = 'invoice'
+        else:
+            type_ = 'credit_note'
+
+        line = {}
+        line['manual'] = False
+        line['description'] = tax.description
+        line['legal_notice'] = tax.legal_notice
+        line['base'] = base
+        line['amount'] = amount
+        line['tax'] = tax.id
+        line['account'] = getattr(tax, '%s_account' % type_).id
+
+        return _TaxKey(**line)
+
+    @staticmethod
+    def _round_taxes(taxes, currency):
+        if not currency:
+            return
+        for taxline in taxes.values():
+            taxline['amount'] = currency.round(taxline['amount'])
